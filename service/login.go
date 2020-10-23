4414d7bf9aa2bfb6e6b1b6d27f185e97e27b3e92
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2020-10-23 16:33:21 +0700

    Add NewLoginService and login service implementation

--- service/login.go
+++ service/login.go
@@ -1,5 +1,18 @@
 package service
 
-type LoginService interface {
-	Login() error
+type (
+	LoginService interface {
+		Login() error
+	}
+
+	loginService struct{}
+)
+
+func NewLoginService() LoginService {
+	return &loginService{}
+}
+
+// Login is handle service. It still bypass checking user
+func (s *loginService) Login() error {
+	return nil
 }
